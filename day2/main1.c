0125d4b6407a29f9342bdad0647f53473fa13099
ekral <ekral@utb.cz>
2021-08-17 10:43:03 +0200

    Update main1.c

--- day2/main1.c
+++ day2/main1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main()
+{
+	// pozor pokrocilejsi priklad
+	int x = 0;
+	printf("Zadej cislo: ");
+	int pocet = scanf_s("%d", &x);
+	int znak = getchar();
+	printf("zadal jsi: %d", x);
+}

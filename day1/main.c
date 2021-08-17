dce897e6639a4884af6437ac11a94d9d0c129e0a
ekral <ekral@utb.cz>
2021-08-17 08:46:26 +0200

    Update main.c

--- day1/main.c
+++ day1/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
 
+int main()
+{
+	int x = 0;
+	int pocetBytuPromenne = sizeof(x);
+	int pocetbytuTypu = sizeof(int);
+	
+	printf("promenna x je ulozena na adrese %p\n", &x);
+	printf("promenna x v pameti zabere %d byty\n", sizeof(x));
+	printf("promenna x ma hodnotu %d\n", x);
+	
+}

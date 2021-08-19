0f2f5eabbf7b4963175be8f26d3a3986d6d91778
ekral <ekral@utb.cz>
2021-08-19 09:16:11 +0200

    Update main_opakovani.c

--- day4/main_opakovani.c
+++ day4/main_opakovani.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+	int x = 3;
+	int druhaMocnina = x * x;
+	printf("%d\n", druhaMocnina);
+
+	double y = 9.0;
+	double druhaOdmocnina = sqrt(y);
+	printf("%lf\n", druhaOdmocnina);
+
+	double z = 2.0;
+	double mocnina = pow(z, 8.0);
+	printf("%lf\n", mocnina);
+
+}

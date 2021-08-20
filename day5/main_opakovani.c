2443ffd59d5260f6b7bffe20a4d44aa3a03e97b8
ekral <ekral@utb.cz>
2021-08-20 09:34:06 +0200

    Update main_opakovani.c

--- day5/main_opakovani.c
+++ day5/main_opakovani.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+	int i = 100;
+	while (i >= 50)
+	{
+		printf("%d ", i);
+		i -= 10;
+	}
+	
+	putchar('\n');
+
+	for (int i = 100; i >= 50; i -= 10)
+	{
+		printf("%d ", i);
+	}
+
+	// 1. Vypiste na konzoli: 100 90 80 70 60 50 
+	// 1a) S pomoci cyklu while
+	// 1b) S pomoci cyklu for
+
+}

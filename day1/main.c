e99fc5cf22670c0126415e8f17b9ff54491cd07c
ekral <ekral@utb.cz>
2022-08-22 11:00:29 +0200

    Add files via upload

--- day1/main.c
+++ day1/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-#include <limits.h>
 
-int main()
+int main() 
 {
-	int x = 0;
-	int pocetBytuPromenne = sizeof(x);
-	int pocetbytuTypu = sizeof(int);
+	// promenna je pojmenovana hodnota v pameti RAM
+	// pamet pro lokalni promennou prirazuje runtime jazyka C automaticky
+	// adresu promenne ziskame pomoci operatoru &, napriklad &datum
+	// kolik bytu zabere promenna v pameti ziskame pomoci klicoveho slova sizeof, napriklad sizeof(datum)
+	int datum = 2002;
+
+	int n1 = sizeof(datum); // kolik bytu zabere promenna datum
+	int n2 = sizeof(int); // kolik bytu zabere typ int
 	
-	printf("promenna x je ulozena na adrese %p\n", &x);
-	printf("promenna x v pameti zabere %d byty\n", sizeof(x));
-	printf("promenna x ma hodnotu %d\n", x);
+	printf("Promenna datum ma hodnotu %d\n", datum);
+	printf("Promenna datum zabere v pameti %d byty\n", sizeof(datum));
+	printf("Promenna datum je ulozena v RAM na adrese %p\n", &datum);
 	
+	// vypiste na terminal, kolik let ma student s datum narozeni v promenne datum
+	int x = 3;
+	int y = 2;
+	int rozdil = x - y;
+
+	int aktualni = 2022;
+
+	return 1;
 }

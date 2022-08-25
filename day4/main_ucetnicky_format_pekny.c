faeb9ab4c828b66461ff93f071fdd25d3bad8b44
ekral <ekral@utb.cz>
2022-08-25 13:26:51 +0200

    Update main_ucetnicky_format_pekny.c

--- day4/main_ucetnicky_format_pekny.c
+++ day4/main_ucetnicky_format_pekny.c
@@ -1,36 +1,55 @@
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 
-int main()
+bool UcetnickyFormat(char* buffer, char* original, int bufferLength)
 {
-	char original[] = "12000000";
-	char kopie[255];
+	int originalLength = strlen(original);
 
-	int delka = strlen(original);
+	int zbytek = originalLength % 3;
+	int pocetTrojic = originalLength / 3;
 
-	int zbytek = delka % 3;
-	int pocetTrojic = delka / 3;
-	
+	if (bufferLength <= originalLength + pocetTrojic)
+	{
+		return false;
+	}
+
 	int i = 0;
 	int j = 0;
 
 	while (i < zbytek)
 	{
-		kopie[j++] = original[i++];
+		buffer[j++] = original[i++];
 	}
-	
-	// TODO vysvetlit rozdil
-	kopie[j++] = ' ';
-	
-	while (i < delka)
-	{
-		kopie[j++] = original[i++];
-		kopie[j++] = original[i++];
-		kopie[j++] = original[i++];
-		kopie[j++] = ' ';
-	}
-	
-	kopie[j - 1] = 0;
 
-	printf("%s\n", kopie);
+	buffer[j++] = ' ';
+
+	while (i < originalLength)
+	{
+		buffer[j++] = original[i++];
+		buffer[j++] = original[i++];
+		buffer[j++] = original[i++];
+		buffer[j++] = ' ';
+	}
+
+	buffer[j - 1] = 0;
+
+	return true;
+}
+
+#define LENGTH 256
+
+int main()
+{
+	double castka = 1000000;
+	char original[LENGTH];
+	char buffer[LENGTH];
+
+	sprintf_s(original, LENGTH, "%.0lf", castka);
+
+	if (UcetnickyFormat(buffer, original, LENGTH))
+	{
+		printf("%s\n", buffer);
+	}
 }

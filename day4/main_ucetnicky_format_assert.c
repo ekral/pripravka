f54f0473527939ac37bee924121cbc57e802546b
ekral <ekral@utb.cz>
2022-08-25 13:26:00 +0200

    Update main_ucetnicky_format_assert.c

--- day4/main_ucetnicky_format_assert.c
+++ day4/main_ucetnicky_format_assert.c
@@ -1,19 +1,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include <stdbool.h>
 
-bool UcetnickyFormat(char* buffer, char* original, int bufferLength)
+void UcetnickyFormat(char* buffer, char* original, int bufferLength)
 {
 	int originalLength = strlen(original);
 
 	int zbytek = originalLength % 3;
 	int pocetTrojic = originalLength / 3;
 
-	if (bufferLength <= originalLength + pocetTrojic)
-	{
-		return false;
-	}
+	assert(bufferLength > originalLength + pocetTrojic);
 	
 	int i = 0;
 	int j = 0;
@@ -36,18 +32,14 @@ bool UcetnickyFormat(char* buffer, char* original, int bufferLength)
 	buffer[j - 1] = 0;
 }
 
-#define LENGTH 256
+#define LENGTH 255
 
 int main()
 {
-	double castka = 1000000;
-	char original[LENGTH];
+	char original[] = "1000000";
 	char buffer[LENGTH];
+	
+	UcetnickyFormat(buffer, original, LENGTH);
 
-	sprintf_s(original, LENGTH, "%.0lf", castka);
-
-	if (UcetnickyFormat(buffer, original, LENGTH))
-	{
-		printf("%s\n", buffer);
-	}
+	printf("%s\n", buffer);
 }

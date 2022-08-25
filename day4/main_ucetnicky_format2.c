f742a348bfdc39b8e7643f4e1de15072ed2145a1
ekral <ekral@utb.cz>
2022-08-25 10:45:19 +0200

    Update main_ucetnicky_format2.c

--- day4/main_ucetnicky_format2.c
+++ day4/main_ucetnicky_format2.c
@@ -19,18 +19,33 @@ int main()
 	// cislo je delitelne beze zbytku 3
 	int zbytek = 9 % 3;
 
-	char retezec[] = "1000000";
+	char original[] = "100500200";
 	
-	int mojeDelka = DelkaRetezce(retezec);
+	int mojeDelka = DelkaRetezce(original);
 
 	// knihovni funkce ze string.h
-	int delka = strlen(retezec);
+	int delka = strlen(original);
 	
 	printf("delka: %d\n", delka);
 
+	char kopie[255];
+	int j = 0;
+
 	for (int i = 0; i < delka; i++)
 	{
-		putchar(retezec[i]);
+		int index = delka - i - 1;
+		
+		kopie[j] = original[i];
+		++j;
+
+		if (index % 3 == 0)
+		{
+			kopie[j] = ' ';
+			++j;
+		}
 	}
 
+	kopie[j] = 0;
+
+	printf("%s\n", kopie);
 }
